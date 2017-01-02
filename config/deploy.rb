f5549de016e015e0bd57c5c26847136a96f7a547
edgardo <edgardo@loventine.com>
2017-01-02 19:24:12 +0100

    Adds capistrano config files

---
+++ config/deploy.rb
@@ -0,0 +1,64 @@
+# config valid only for current version of Capistrano
+lock '3.7.1'
+
+set :application, 'uploads'
+set :repo_url, "88.198.48.241:/repositories/#{fetch(:application)}.git"
+set :branch, 'master'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+set :pty, false
+
+set :ssh_options, forward_agent: true
+
+# Default value for :linked_files is []
+custom_linked_files = %w(
+  config/database.yml
+)
+set :linked_files, fetch(:linked_files, []) + custom_linked_files
+
+# Default value for linked_dirs is []
+custom_linked_dirs = %w(
+  log
+  tmp/pids
+  tmp/cache
+  tmp/sockets
+  public/system
+  public/uploads
+)
+set :linked_dirs, fetch(:linked_dirs, []) + custom_linked_dirs
+
+# Defaults to false
+# Skip migration if files in db/migrate were not modified
+set :conditionally_migrate, true
+
+# Defaults to nil (no asset cleanup is performed)
+# If you use Rails 4+ and you'd like to clean up old assets after each deploy,
+# set this to the number of versions to keep
+set :keep_assets, 2
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,70 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
+# server "db.example.com", user: "deploy", roles: %w{db}
+
+server '88.99.69.71', user: 'deployer', roles: %w(web app db)
+
+# role-based syntax
+# ==================
+
+# Defines a role with one or multiple servers. The primary server in each
+# group is considered to be the first unless any hosts have the primary
+# property set. Specify the username and a domain or IP for the server.
+# Don't use `:all`, it's a meta role.
+
+role :web, '88.99.69.71'
+role :app, '88.99.69.71'
+role :db, '88.99.69.71'
+
+# Configuration
+# =============
+# You can set any configuration variable like in config/deploy.rb
+# These variables are then only loaded and set in this stage.
+# For available Capistrano configuration variables see the documentation page.
+# http://capistranorb.com/documentation/getting-started/configuration/
+# Feel free to add new variables to customise your setup.
+
+
+# Vars
+set :deploy_to, "/var/www/html/#{fetch(:application)}_staging"
+
+set :unicorn_user, fetch(:user)
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+set :unicorn_config, "#{shared_path}/config/unicorn.rb"
+set :unicorn_log, "#{shared_path}/log/unicorn.log"
+set :unicorn_workers, 2
+set :unicorn_name, "#{fetch(:application)}_staging"
+
+set :nginx_name, "#{fetch(:application)}_staging"
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult the Net::SSH documentation.
+# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# The server-based syntax can be used to override options:
+# ------------------------------------
+# server "example.com",
+#   user: "user_name",
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: "user_name", # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: "please use keys"
+#   }
