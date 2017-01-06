1f6a6686b00a534bae98055d9faec0e3e4d86188
edgardo <edgardo@loventine.com>
2017-01-06 20:25:34 +0100

    removes redis_server from sidekiq initializer (this doesnt work - it is ignored)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,11 @@
+redis_server = Rails.env.development? ? '127.0.0.1' : '88.99.69.71'
+redis_database = Rails.env.staging? ? '1' : '0'
+
 Sidekiq.configure_server do |config|
   config.server_middleware.remove Sidekiq::Middleware::Server::RetryJobs
-  config.redis = { url: Figaro.env.redis_server, password: Figaro.env.redis_password }
+  config.redis = { url: "redis://#{redis_server}:6379/#{redis_database}", password: Figaro.env.redis_password }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Figaro.env.redis_server, password: Figaro.env.redis_password }
+  config.redis = { url: "redis://#{redis_server}:6379/#{redis_database}", password: Figaro.env.redis_password }
 end
