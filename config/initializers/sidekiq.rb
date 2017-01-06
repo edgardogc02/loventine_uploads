5a67dec5c23f123357e11bb0bc9022dfa101bedb
edgardo <edgardo@loventine.com>
2017-01-06 20:31:17 +0100

    removes redis_server from sidekiq initializer (this doesnt work - it is ignored)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 redis_server = Rails.env.development? ? '127.0.0.1' : '88.99.69.71'
-redis_database = Rails.env.staging? ? '1' : '0'
+redis_database = Rails.env.production? ? '1' : '0'
 
 Sidekiq.configure_server do |config|
   config.server_middleware.remove Sidekiq::Middleware::Server::RetryJobs
